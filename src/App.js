d957f4090c3256309cbbefafeae74083c9294b4c
Rohit Joshi <rohit.joshiadvanced@gmail.com>
2022-08-12 16:26:01 +0530

    Final commit of tutorial with useReducer and localStorage

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import NewBookForm from './components/BookForm';
+import BookList from './components/BookList';
 import Navbar from './components/Navbar';
 import BookContextProvider from './contexts/BookContext';
 
@@ -6,6 +8,8 @@ function App() {
     <div className='App'>
     <BookContextProvider>
       <Navbar />
+      <BookList />
+      <NewBookForm />
     </BookContextProvider>
     </div>
   );
